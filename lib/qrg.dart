daef2667c650fb7b6360bbd8ed2885efa412540e
Gnubz <matahareek2@gmail.com>
2022-07-12 17:15:56 +0700

    qrscan

---
+++ lib/qrg.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class Qr extends StatefulWidget {
+  Qr({Key? key}) : super(key: key);
+
+  @override
+  State<Qr> createState() => _QrState();
+}
+
+class _QrState extends State<Qr> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            QrImage(
+              data: controller.text,
+              version: QrVersions.auto,
+              size: 200.0,
+            ),
+            buildtextfild(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextField buildtextfild(BuildContext context) => TextField(
+        controller: controller,
+        style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 20),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'QR',
+          labelStyle: TextStyle(color: Colors.white),
+          hintText: 'QR',
+          hintStyle: TextStyle(color: Colors.white),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(255, 240, 2, 2)),
+          ),
+          suffix: IconButton(
+            color: Theme.of(context).colorScheme.secondary,
+            icon: Icon(Icons.camera_alt),
+            onPressed: () {
+              scan();
+            },
+          ),
+        ),
+      );
+  scan() {
+    setState(() {});
+  }
+}
---
+++ lib/qrscan.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class Qrscan extends StatefulWidget {
+  Qrscan({Key? key}) : super(key: key);
+
+  @override
+  State<Qrscan> createState() => _QrscanState();
+}
+
+class _QrscanState extends State<Qrscan> {
+  Barcode? barcode;
+  QRViewController? controller;
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Qrscan'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          buildQrView(context),
+          Positioned(bottom: 10, child: buildResult()),
+          Positioned(
+            top: 10,
+            child: buildcot(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildcot() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          IconButton(
+              icon: (Icon(Icons.camera_alt)),
+              onPressed: () {
+                controller!.pauseCamera();
+              }),
+        ],
+      );
+  Widget buildResult() => Container(
+        padding: EdgeInsets.all(24),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: Colors.white24),
+        child: Text(
+          barcode != null ? 'Result : ${barcode!.code}' : 'scan a code',
+          maxLines: 3,
+        ),
+      );
+  Widget buildQrView(BuildContext context) => QRView(
+        key: qrKey,
+        onQRViewCreated: _onQrViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderColor: Theme.of(context).colorScheme.secondary,
+          borderRadius: 10,
+          borderLength: 20,
+          borderWidth: 10,
+          cutOutSize: MediaQuery.of(context).size.width * 0.8,
+        ),
+      );
+  void _onQrViewCreated(QRViewController controller) {
+    setState(() => this.controller = controller);
+    controller.scannedDataStream
+        .listen((barcode) => setState(() => this.barcode = barcode));
+  }
+}
