0c8c86c83e3d38fa942c7f9761662445277b2849
Gnubz <matahareek2@gmail.com>
2022-07-13 13:36:20 +0700

    test

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_01/qrg.dart';
 import 'package:flutter_01/qrscan.dart';
-
 import 'package:flutter_01/test02.dart';
 
 //test 010101//
@@ -13,7 +12,7 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
-  List<Widget> listwidget = [const Test02(), Qr(), Qrscan()];
+  List<Widget> listwidget = [const Test02(),  Qr(),  Qrscan()];
 
   int index = 0;
 
@@ -22,7 +21,6 @@ class _MenuState extends State<Menu> {
       this.index = index;
     });
   }
-  
 
   @override
   Widget build(BuildContext context) {
@@ -31,34 +29,42 @@ class _MenuState extends State<Menu> {
       bottomNavigationBar: bottom01(),
     );
   }
+
   BottomNavigationBarItem bottomNavigationBarItem() {
     return const BottomNavigationBarItem(
-        icon: Icon(Icons.local_hospital_outlined),
-        label: 'Covid',
-        backgroundColor: Colors.pink,);
+      icon: Icon(Icons.local_hospital_outlined),
+      label: 'Covid',
+      backgroundColor: Colors.pink,
+    );
   }
+
   BottomNavigationBarItem bottomNavigationBarItem2() {
     return const BottomNavigationBarItem(
-        icon: Icon(Icons.business),
-        label: 'Qr gen',
-        backgroundColor: Color.fromARGB(255, 240, 236, 236),);
-        }
+      icon: Icon(Icons.business),
+      label: 'Qr gen',
+      backgroundColor: Color.fromARGB(255, 240, 236, 236),
+    );
+  }
 
-   BottomNavigationBarItem bottomNavigationBarItem3() {
+  BottomNavigationBarItem bottomNavigationBarItem3() {
     return const BottomNavigationBarItem(
-        icon: Icon(Icons.camera_alt),
-        label: 'Qr Scan',
-        backgroundColor: Color.fromARGB(255, 240, 236, 236),);
-        }
+      icon: Icon(Icons.camera_alt),
+      label: 'Qr Scan',
+      backgroundColor: Color.fromARGB(255, 228, 66, 66),
+    );
+  }
 
   BottomNavigationBar bottom01() {
     return BottomNavigationBar(
-      items: [bottomNavigationBarItem(), bottomNavigationBarItem2(),bottomNavigationBarItem3()],
+      items: [
+        bottomNavigationBarItem(),
+        bottomNavigationBarItem2(),
+        bottomNavigationBarItem3()
+      ],
       currentIndex: index,
       selectedItemColor: Colors.amber[500],
       backgroundColor: const Color.fromARGB(255, 54, 244, 155),
       onTap: onitem,
-      
     );
   }
 }
